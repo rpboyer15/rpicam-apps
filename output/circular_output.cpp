f136655ff89fc4352a66aae38997f88a2dc4f250
Reed Boyer <rpboyer15@gmail.com>
2025-05-29 23:21:06 -0700

    test

--- output/circular_output.cpp
+++ output/circular_output.cpp
@@ -28,53 +28,16 @@ struct Header
 static_assert(sizeof(Header) % ALIGN == 0, "Header should have aligned size");
 
 // Size of buffer (options->circular) is given in megabytes.
-CircularOutput::CircularOutput(VideoOptions const *options) : Output(options), cb_(options->circular << 20)
+CircularOutput::CircularOutput(VideoOptions const *options)
+	: Output(options), cb_(options->circular << 20), fp_(nullptr)
 {
-	// Open this now, so that we can get any complaints out of the way
 	if (options_->output == "-")
 		fp_ = stdout;
-	else if (!options_->output.empty())
-	{
-		fp_ = fopen(options_->output.c_str(), "w");
-	}
-	if (!fp_)
-		throw std::runtime_error("could not open output file");
 }
 
 CircularOutput::~CircularOutput()
 {
-	unsigned int total = 0, frames = 0;
-	bool seen_keyframe = false;
-	Header header;
-	FILE *fp = fp_; // can't capture a class member in a lambda
-
-	while (!cb_.Empty())
-	{
-		uint8_t *dst = (uint8_t *)&header;
-		cb_.Read(
-			[&dst](void *src, int n)
-			{
-				memcpy(dst, src, n);
-				dst += n;
-			},
-			sizeof(header));
-
-		seen_keyframe |= header.keyframe;
-		if (seen_keyframe)
-		{
-			cb_.Read([fp](void *src, int n) { fwrite(src, 1, n, fp); }, header.length);
-			cb_.Skip((ALIGN - header.length) & (ALIGN - 1));
-			total += header.length;
-			if (fp_timestamps_)
-				Output::timestampReady(header.timestamp);
-			frames++;
-		}
-		else
-			cb_.Skip((header.length + ALIGN - 1) & ~(ALIGN - 1));
-	}
-
-	fclose(fp_);
-	LOG(1, "Wrote " << total << " bytes (" << frames << " frames)");
+	// Do nothing. All dumping is handled via DumpToFile()
 }
 
 void CircularOutput::outputBuffer(void *mem, size_t size, int64_t timestamp_us, uint32_t flags)
@@ -111,6 +74,12 @@ void CircularOutput::timestampReady(int64_t timestamp)
 
 void CircularOutput::DumpToFile()
 {
+	if (options_->output.empty() || options_->output == "-")
+	{
+		std::cerr << "Output filename is not set or is stdout; cannot dump circular buffer.\n";
+		return;
+	}
+
 	// Generate a unique filename with timestamp
 	auto now = std::chrono::system_clock::now();
 	std::time_t t = std::chrono::system_clock::to_time_t(now);
@@ -128,12 +97,11 @@ void CircularOutput::DumpToFile()
 	bool seen_keyframe = false;
 	Header header;
 
-	CircularBuffer temp_cb = cb_; // Copy the circular buffer so we don't consume it
-
-	while (!temp_cb.Empty())
+	// Use real buffer (not copy!)
+	while (!cb_.Empty())
 	{
 		uint8_t *dst = (uint8_t *)&header;
-		temp_cb.Read(
+		cb_.Read(
 			[&dst](void *src, int n)
 			{
 				memcpy(dst, src, n);
@@ -144,15 +112,15 @@ void CircularOutput::DumpToFile()
 		seen_keyframe |= header.keyframe;
 		if (seen_keyframe)
 		{
-			temp_cb.Read([fp](void *src, int n) { fwrite(src, 1, n, fp); }, header.length);
-			temp_cb.Skip((ALIGN - header.length) & (ALIGN - 1));
+			cb_.Read([fp](void *src, int n) { fwrite(src, 1, n, fp); }, header.length);
+			cb_.Skip((ALIGN - header.length) & (ALIGN - 1));
 			total += header.length;
 			if (fp_timestamps_)
 				Output::timestampReady(header.timestamp);
 			frames++;
 		}
 		else
-			temp_cb.Skip((header.length + ALIGN - 1) & ~(ALIGN - 1));
+			cb_.Skip((header.length + ALIGN - 1) & ~(ALIGN - 1));
 	}
 
 	fclose(fp);
